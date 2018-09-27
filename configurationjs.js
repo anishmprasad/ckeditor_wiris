d04ae1b09a4af2971cded909f9c17a197c798e48
anish.m.prasad <anish.m.prasad@gmail.com>
2018-09-27 22:58:02 +0530

    Initial commit

---
+++ configurationjs.js
@@ -0,0 +1,26 @@
+{
+"_wrs_conf_editorUrl": "http://www.wiris.net/demo/editor/editor",
+"_wrs_conf_hostPlatform": "unknown",
+"_wrs_conf_wirisPluginPerformance": true,
+"_wrs_conf_modalWindow": true,
+"_wrs_conf_modalWindowFullScreen": false,
+"_wrs_conf_editorEnabled": true,
+"_wrs_conf_CASMathmlAttribute": "alt",
+"_wrs_conf_editorToolbar": "",
+"_wrs_conf_saveMode": "xml",
+"_wrs_conf_imageFormat": "svg",
+"_wrs_conf_chemEnabled": true,
+"_wrs_conf_versionPlatform": "unknown",
+"_wrs_conf_CASEnabled": false,
+"_wrs_conf_parseModes": [
+"latex"
+],
+"_wrs_conf_enableAccessibility": true,
+"_wrs_conf_version": "4.12.0.1393",
+"_wrs_conf_setSize": false,
+"_wrs_conf_editMode": "default",
+"_wrs_conf_imageMathmlAttribute": "data-mathml",
+"_wrs_conf_editorParameters": {},
+"_wrs_conf_editorAttributes": "width=570, height=450, scroll=no, resizable=yes",
+"_wrs_conf_CASAttributes": "width=640, height=480, scroll=no, resizable=yes"
+}
